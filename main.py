4a3d0b9df2c86449879be7fd13d8f59261818622
Blake Engelbrecht <bengelbrecht95@gmail.com>
2021-02-24 17:38:00 -0600

    project initialization

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Project:            TicTacToe Minimax
+Description:        Unbeatable A.I. bot using a minimax algorithm implementation
+Contributors:       Blake Engelbrecht
+Date:               2/26/2021
+"""
+
